e3980cdb27936564f97dff8f9ef0e07b139f62af
hydrox19 <hydrox19@yandex.com>
2021-12-14 21:15:13 +0530

    fix(makeRequest): more logic

--- src/utils/makeRequest.ts
+++ src/utils/makeRequest.ts
@@ -2,7 +2,9 @@ import centra from "@aero/centra";
 import { HttpMethod, SendOptions, HookClientOptions } from "../typings";
 
 export async function makeRequest(url: string, requestType: HttpMethod, body: SendOptions, clientOptions: HookClientOptions): Promise<any> {
-    const res = centra(url, requestType).body(body, "json") as any;
+    let res = centra(url, requestType).body(body, "json") as any;
+    if (["PATCH", "POST"].includes(requestType)) res.header("Content-Type", "application/json");
+    res = await res.send("form");
     if (res.statusCode === 429 && clientOptions.retryOnLimit) {
         const retryAfter = res.body["retry_after"] as number;
         setTimeout(() => makeRequest(url, requestType, body, clientOptions), retryAfter);
